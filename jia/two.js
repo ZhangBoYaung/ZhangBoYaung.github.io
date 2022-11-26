bd3ee22a1a63a4c9dbdcc30393f76e3e01900225
ZhangBoYaung <116617723+ZhangBoYaung@users.noreply.github.com>
2022-11-26 19:27:41 +0800

    Add files via upload

---
+++ jia/two.js
@@ -0,0 +1,11 @@
+window.alert("提示：如果出现网页加载缓慢的情况\n请一定要耐心等待\n如果出现无法连接的情况下\n请进行多次刷新!");
+function xianshi()
+{
+    ci=document.getElementById("changci1")
+    ci.innerHTML="[二黄导板]眼睁睁气数到金汤未稳<br/>[二黄碰板]自登基 东也荒 西也旱 无一日得到安宁<br/>[二黄原板]听说是居庸关贼兵围困<br/>三百年锦江山化为灰尘<br/>满朝中俱都是谗臣奸佞<br/>哪一个能分忧能定太平<br/>可怜我一统封疆被流枭吞并"
+}
+function xiaochu()
+{
+    ci=document.getElementById("changci1")
+    ci.innerHTML=' '
+}
