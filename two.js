f1a61f2504565b71a484df4718bc39d4bc072188
ZhangBoYaung <youhulu2021@outlook.com>
2023-10-03 23:32:40 +0800

    提交notes

--- two.js
+++ two.js
@@ -1,19 +1,4 @@
 window.alert("你好！\n这里是张博洋的网站！");
-function xianshi()
-{
-    ci=document.getElementById("changci1");
-    ci.innerHTML="[二黄导板]眼睁睁气数到金汤未稳<br/>[二黄碰板]自登基 东也荒 西也旱 无一日得到安宁<br/>[二黄原板]听说是居庸关贼兵围困<br/>三百年锦江山化为灰尘<br/>满朝中俱都是谗臣奸佞<br/>哪一个能分忧能定太平<br/>可怜我一统封疆被流枭吞并";
-}
-function xiaochu()
-{
-    ci=document.getElementById("changci1");
-    ci.innerHTML=' ';
-}
-function kuangjia()
-{
-    k=document.getElementById("kjds");
-    k.src="three.htm";
-}
 function butiaozhuan()
 {
     document.getElementById("div").style='display:none';
@@ -25,4 +10,10 @@ function tiaozhuan()
 function code()
 {
     document.getElementById("div").style.display='block';
+}
+function hello()
+{
+    var txt = prompt("输入0至100的任意一个整数或小数")
+   var a = document.getElementById("hello");
+   a.style='font-size:25px;animation-name:welcome2;animation-duration:'+txt+'s;animation-iteration-count:infinite; position:fixed;';
 }
